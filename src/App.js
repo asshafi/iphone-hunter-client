d1d8360b5c77ca8b9f28ad03c34ae9fd42cead15
shafi <asshafi15@gmail.com>
2022-10-12 22:03:28 +0600

    using own api data on  client site

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Main from './Layout/Main';
+import Home from './Components/Home';
+import Phone from './Components/Phone';
 
 function App() {
+  const router=createBrowserRouter([
+    {
+      path: '/',
+      element: <Main></Main>,
+      children: [
+        {
+          path: '/',
+          element:<Home></Home>,
+          loader:()=> fetch('http://localhost:5000/phones')
+        },
+        {
+          path: '/phones/:id',
+          element: <Phone></Phone>
+        }
+      ]
+    }
+  ])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
---
+++ src/Components/Phone.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Phone = () => {
+    return (
+        <div>
+            <h2>Single phone</h2>
+        </div>
+    );
+};
+
+export default Phone;
